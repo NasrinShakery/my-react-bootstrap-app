4e4e0e0b1fc3257f4864d010498afc1dd6bf76f1
NasrinShakery <nasrin.shakery8@gmail.com>
2023-10-30 13:56:42 +0330

    style: header img

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import Image from 'react-bootstrap/Image';
+
+function Header() {
+  return (
+   <>
+      {/* <Image src="./images/nail-polish.png" fluid /> */}
+      <Image src="./images/beauty.png" fluid />
+   </>
+  )
+}
+
+export default Header;
